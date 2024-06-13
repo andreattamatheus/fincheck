6ee159e62ff35e3181cf400cb4d280a45e4fb00a
Matheus Andreatta <matheus.andreatta@pdmfc.com>
2024-06-13 08:37:13 -0300

    [FRONT] implement createContext and useContext to user login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import { Router } from "./Router";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./app/contexts/AuthContext";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <Toaster />
+      </QueryClientProvider>
+    </AuthProvider>
   );
 }
 
